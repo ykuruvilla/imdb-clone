681057e1096bc05b20968f1f1823ffe8e67bf5f1
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-05-18 23:46:33 +0100

    create and style about page

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -6,7 +6,7 @@ import DarkModeSwitch from "../DarkModeSwitch/DarkModeSwitch";
 
 const Header = () => {
   return (
-    <header className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center">
+    <header className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="about" Icon={BsFillInfoCircleFill} />
